9a3ab395c204d1a17e1c19091d94281ec8db2cd1
Ahmed Masoud <75045445+Ahmed-masou@users.noreply.github.com>
2022-12-01 06:40:46 +0200

    Create Dart Comments.dart

---
+++ Dart Comments.dart
@@ -0,0 +1,12 @@
+// Single Line Comments
+void main(){  
+    // This will print the given statement on screen  
+    print("Welcome to Ahmed Repo");  
+  
+      /* This is the example of multi-line comment 
+    This will print the given statement on screen */  
+      
+    print("Welcome Back gusy :)");  
+}  
+
+
